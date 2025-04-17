2dbe82bf612bd32d51de41c4cdfeea7c5c010daf
Cyprien Kelma <cyprien.kelma@gmail.com>
2025-04-17 11:22:03 +0200

    .

--- pipeline/workflows/proof_of_concept/script/bronze_to_silver.py
+++ pipeline/workflows/proof_of_concept/script/bronze_to_silver.py
@@ -6,6 +6,7 @@ spark = SparkSession.builder \
     .config("spark.hadoop.fs.s3a.access.key", "minio") \
     .config("spark.hadoop.fs.s3a.secret.key", "minio123") \
     .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+    .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "false") \
     .getOrCreate()
 
 # df = spark.read.csv("s3a://bronze/demo/users.csv", header=True)
