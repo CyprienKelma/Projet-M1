9fd35b171ae42486ac323f35fe49f171673bf305
Cyprien Kelma <cyprien.kelma@gmail.com>
2025-04-17 12:46:33 +0200

    .

--- pipeline/workflows/proof_of_concept/script/bronze_to_silver.py
+++ pipeline/workflows/proof_of_concept/script/bronze_to_silver.py
@@ -2,14 +2,14 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder \
     .appName("bronze-to-silver-demo") \
-    .config("spark.hadoop.fs.s3a.endpoint", "http://minio-tenant-pool-0-0.minio-tenant-hl.minio-tenant.svc.cluster.local:9000") \
+    .config("spark.hadoop.fs.s3a.endpoint", "http://minio-s3a.minio-tenant:9000") \
     .config("spark.hadoop.fs.s3a.access.key", "minio") \
     .config("spark.hadoop.fs.s3a.secret.key", "minio123") \
     .config("spark.hadoop.fs.s3a.path.style.access", "true") \
     .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "false") \
     .getOrCreate()
 
-df = spark.read.csv("s3a://bronze/demo/users.csv", header=True)
+df = spark.read.csv("s3a://minio-s3a.minio-tenant:9000/bronze/demo/users.csv", header=True)
 df_clean = df.filter(df["email"].isNotNull())
 df_clean.write.mode("overwrite").parquet("s3a://silver/demo/users_clean.parquet")
 
