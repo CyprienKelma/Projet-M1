20921d82fe7fab0c9ecd0978166bdc7ba6c38926
Cyprien Kelma <cyprien.kelma@gmail.com>
2025-04-17 12:51:12 +0200

    .

--- pipeline/workflows/proof_of_concept/script/bronze_to_silver.py
+++ pipeline/workflows/proof_of_concept/script/bronze_to_silver.py
@@ -9,9 +9,9 @@ spark = SparkSession.builder \
     .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "false") \
     .getOrCreate()
 
-df = spark.read.csv("s3a://minio-s3a.minio-tenant:9000/bronze/demo/users.csv", header=True)
-df_clean = df.filter(df["email"].isNotNull())
-df_clean.write.mode("overwrite").parquet("s3a://silver/demo/users_clean.parquet")
+# df = spark.read.csv("s3a://minio-s3a.minio-tenant:9000/bronze/demo/users.csv", header=True)
+# df_clean = df.filter(df["email"].isNotNull())
+# df_clean.write.mode("overwrite").parquet("s3a://silver/demo/users_clean.parquet")
 
 
 print("Transfortmation réussie pour Bronze ---> Silver")
