084203e13d46000278a9f139641bb14e7c9f3f25
Cyprien Kelma <cyprien.kelma@gmail.com>
2025-04-20 15:45:14 +0200

    .

--- pipeline/workflows/prod_pipeline/silver_to_notif_impact.py
+++ pipeline/workflows/prod_pipeline/silver_to_notif_impact.py
@@ -42,9 +42,6 @@ def transform_silver_to_notif_impact(**context):
 
     print("Notif Shape : ", notif_states.shape, notif_states.head())
     print("Activity Shape : ", user_notifs.shape, user_notifs.head())
-    
-    merged["user_id"] = merged["user_id"].astype(str)
-    sessions["user_id"] = sessions["user_id"].astype(str)
 
 
     # Supposons que si status == 'seen' c’est un succès
@@ -55,6 +52,9 @@ def transform_silver_to_notif_impact(**context):
         left_on=["notification_id", "user_id"],
         right_on=["notification_id", "user_id"]
     )
+
+    merged["user_id"] = merged["user_id"].astype(str)
+    sessions["user_id"] = sessions["user_id"].astype(str)
 
     merged["is_success"] = merged["status"] == "read"
 
